6844600bcf5db089ecbf5c88ad892d3cec4abd29
chirag <chiragiem36@gmail.com>
2019-01-09 17:45:51 +0530

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    main: __dirname + '/src/index.js'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist',
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
+}
