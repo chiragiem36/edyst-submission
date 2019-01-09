9b31a659f553715c3ffea2c2525324d451fc609e
chirag <chiragiem36@gmail.com>
2019-01-09 18:15:04 +0530

    added index file. Fetching pokemon cards on browser load and storing in store

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const xhr = require('./js/xhr.js')
+const store = require('./js/store.js')
+const state = store.state()
+
+window.onload = () => {
+  const options = {
+    url: "https://dev-util.edyst.com/challenge/pokemons/random"
+  }
+  xhr.get(options)
+  .then((res) => {
+    store.setState('cards', res.body)
+  })
+  .then(() => {
+  })
+}
